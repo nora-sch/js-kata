26d9a80959532da1503dcac6ad30aa5195c0b463
nora-sch <nora.schumann.development@gmail.com>
2023-04-25 10:35:46 +0200

    social-like

--- src/array/social-like/social-like.js
+++ src/array/social-like/social-like.js
@@ -9,4 +9,22 @@ Create a function `getLikes` which receives an array of names, and returns:
 
 // TODO add your code here
 
+function getLikes(arrayOfNames) {
+  let result;
+  switch (arrayOfNames.length) {
+    case 0:
+      result = "Be the first to like this";
+      break;
+    case 1:
+      result = `${arrayOfNames[0]} likes this`;
+      break;
+    case 2:
+      result = `${arrayOfNames[0]} and ${arrayOfNames[1]} like this`;
+      break;
+    default:
+      result = `${arrayOfNames[0]} and ${arrayOfNames.length - 1} other people like this`;
+  }
+  return result;
+}
+
 module.exports = getLikes;
