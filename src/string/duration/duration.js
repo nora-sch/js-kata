60425eec05f46bacc9ca9e8843d0eee221b7edfa
nora-sch <nora.schumann.development@gmail.com>
2023-04-25 12:05:24 +0200

    duration

--- src/string/duration/duration.js
+++ src/string/duration/duration.js
@@ -13,4 +13,14 @@ Add you own tests.
 
 // TODO add your code here
 
+function convertTime(timeString){
+    if (timeString.match(/^[0-9]{2}:[0-9]{2}$/)) {
+        timeArray = timeString.split(":");  
+        console.log(timeArray);
+        return parseInt(timeArray[0]) * 60 + parseInt(timeArray[1]);
+    } else {
+        return null;
+    }
+}
+// console.log(convertTime("01h45m"));
 module.exports = convertTime;
--- src/string/duration/duration.test.js
+++ src/string/duration/duration.test.js
@@ -9,3 +9,16 @@ test("convertTime as one parameter", () => {
 });
 
 // TODO add your tests here
+
+// * "02:30" -> 150
+test("convertTime normal case", () => {
+  expect(convertTime("02:30")).toBe(150);
+});
+// * "01:45" -> 105
+test("convertTime format incorrect 1", () => {
+  expect(convertTime("025:30")).toBe(null);
+});
+// * "01h45m" -> null
+test("convertTime format incorrect 2", () => {
+  expect(convertTime("01h45m")).toBe(null);
+});
