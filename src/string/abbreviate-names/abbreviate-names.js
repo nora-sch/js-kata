6510de366c67d1fb77bd9b2c60aff0f1422231be
nora-sch <nora.schumann.development@gmail.com>
2023-04-25 11:15:00 +0200

    abbreviate-names

--- src/string/abbreviate-names/abbreviate-names.js
+++ src/string/abbreviate-names/abbreviate-names.js
@@ -14,4 +14,14 @@ Add you own tests.
 
 // TODO add your code here
 
-module.exports = abbreviate;
+function abbreviate (name) {
+    const arrayOfNames = name.split(" ");
+    let abbreviation = "";
+    arrayOfNames.forEach((name) => {
+        abbreviation = abbreviation + "." + name[0] ;
+    });
+    return abbreviation.slice(1);
+}
+
+// console.log(abbreviate("Alyson Hannigan"))
+ module.exports = abbreviate;
